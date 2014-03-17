ba1e664384bae784fc4149d85d9c4ecbc3d282b5
dontknowwhy <anakilee(a)google_com>
2014-03-17 15:33:39 +0800

    整理对sql database的版本控制，补充kendoui�

---
+++ Northwind.Mvc5App/Areas/Warehouse/WarehouseAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Northwind.Mvc5App.Areas.Warehouse
+{
+    public class WarehouseAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Warehouse";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Warehouse_default",
+                "Warehouse/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
